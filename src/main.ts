c52c1232e5356f917c7d22767d9f45d21ea2e83f
aersamkull <ehrsam@kull.ch>
2016-04-19 16:48:46 +0200

    Some seupt changes

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@
 
 import fs = require("fs");
 import yargs = require("yargs");
+import worker = require("./worker");
 
 const fileName = yargs.argv.fileName || "E:\\Code\\Kull\\P0939FHNW\\FHNW_Web_FilePermissions-dev\\FHNW_Web_FilePermissions\\Views\\Admin\\Administrators.cshtml";
-
 const fileContent = fs.readFileSync(fileName, "utf-8");
-const regex1 = new RegExp(`data[-]bind=["][^"]*["]`, "g");
-const regex2 = new RegExp(`data[-]bind=['][']*[']`, "g");
-const allMatches = fileContent.match(regex1).concat(fileContent.match(regex2));
+const allMatches = worker.parseBindings(fileContent);
 allMatches.forEach(s=> console.log(s));
-
+  
+if(yargs.argv.output) {
+    fs.writeFileSync(yargs.argv.output, worker.generatePreScriptContent(allMatches), {encoding: "utf-8"})
+}
---
+++ src/worker.ts
@@ -0,0 +1,40 @@
+
+
+export function parseBindings(content: string) {
+    const regex1 = new RegExp(`data[-]bind=["][^"]*["]`, "g");
+    const regex2 = new RegExp(`data[-]bind=['][']*[']`, "g");
+    const allMatches = content.match(regex1).concat(content.match(regex2));
+    return allMatches.filter(s=>s!=null).map(s=> s.substring("data-bind=".length+1, s.length - 1));
+}
+export function getFunctionString(binding: string) {
+    return "function($context, $element) { with($context){with($data||{}){return{" + binding + "}}}  }";
+}
+export function getBindingKey(binding: string){
+    return binding + "true";
+}
+
+export function generateKnockoutStr(bindings: string[]) {
+    return "{" + bindings.reduce((pv, vl, ind)=> {
+        return pv + (ind? ", ": "") + JSON.stringify(getBindingKey(vl)) + ":" + getFunctionString(vl);
+    }, "") + "}";
+}
+
+export function generatePreScriptContent(bindings: string[]) {
+    
+    
+    return `
+    (()=>{
+        var obj = ${generateKnockoutStr(bindings)};
+        var knockoutCache = ko.bindingProvider.instance.bindingCache;
+        if(!knockoutCache) {
+            // It's minified. Lets do an ugly hack
+            knockoutCache=ko.bindingProvider.instance[Object.keys(new ko.bindingProvider())[0]];
+        }
+        for(var key in obj) {
+            if(obj.hasOwnProperty(key)) {
+                ko.bindingProvider.instance.bindingCache[key] = obj[key];
+            }
+        }
+    })
+    `;
+}
