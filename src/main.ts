591bb3b728f99e20f15be7ad2fbcede5d6a7f691
Adrian Ehrsam <ehrsam@kull.ch>
2020-05-04 14:24:34 +0200

    some package updates

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import fs = require("fs");
 import yargs = require("yargs");
 import worker = require("./worker");
 
-const fileName = yargs.argv.fileName || "../test/test.html";
+const fileName = yargs.argv.fileName as string || "../test/test.html";
 const fileContent = fs.readFileSync(fileName, "utf-8");
 const allMatches = worker.parseBindings(fileContent);
  
-const outputFileName = yargs.argv.output || (fileName === "../test/test.html" ? "../dist/test.js" : null);
+const outputFileName = yargs.argv.output as string || (fileName === "../test/test.html" ? "../dist/test.js" : null);
   
 if(outputFileName) {
     fs.writeFileSync(outputFileName, worker.generatePreScriptContent(allMatches), {encoding: "utf-8"});
